402c0fadd3b671e69a9ddc954566b0731fc0ce54
ckhe1215 <ckhe1215@gmail.com>
2021-03-30 23:17:54 +0900

    index화면 생성

---
+++ src/main/java/haeun/kim/surveyproject/Application.java
@@ -0,0 +1,11 @@
+package haeun.kim.surveyproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
